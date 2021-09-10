c7897d1789d0e73ff7f38514b798ed2d03eafabe
itzstory7 <85459808+itzstory7@users.noreply.github.com>
2021-09-10 16:01:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 hostname localhost
-yum install httpd sudo -y
+yum install httpd sudo wget unzip -y
 yum install mariadb-server -y
 service mariadb start
 mysql_secure_installation
@@ -13,8 +13,7 @@ rm -rf gcpcentos.zip
 chmod -R 755 gcp
 
 clear
-echo “
-127.0.0.1 billing.gamecp.com billing1.gamecp.com billing2.gamecp.com
+echo “127.0.0.1 billing.gamecp.com billing1.gamecp.com billing2.gamecp.com
 89.163.132.56” >> /etc/hosts
 
 
@@ -27,12 +26,11 @@ cd /gcp
 
 sh c1.sh
 
-clear
 
 cd /usr/local/gcp
 cd installs
 mkdir -p cs && cd cs
-wget http://albaneso.eu/hlds/hlds.zip/etrail.zip
+wget http://albaneso.eu/hlds/etrail.zip
 yum install unzip -y
 unzip etrail.zip
 rm -rf etrail.zip
